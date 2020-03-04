81612347ac2ad3fc568837bbf510169732771c52
Kmbariah <Kmbaria@github.com>
2020-03-04 12:46:49 +0300

    made all changes on the home page and added styling

--- js/scripts.js
+++ js/scripts.js
@@ -1,3 +1,4 @@
+<<<<<<< HEAD
 
 $(document).ready(function(){
   $("h3#hmajor").click(function(){
@@ -241,3 +242,23 @@ $("document").ready(function(){
         });
       });
       
+=======
+var mybutton = document.getElementById("myBtn");
+
+// When the user scrolls down 20px from the top of the document, show the button
+window.onscroll = function() {scrollFunction()};
+
+function scrollFunction() {
+  if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
+    mybutton.style.display = "block";
+  } else {
+    mybutton.style.display = "none";
+  }
+}
+
+// When the user clicks on the button, scroll to the top of the document
+function topFunction() {
+  document.body.scrollTop = 0;
+  document.documentElement.scrollTop = 0;
+}
+>>>>>>> made all changes on the home page and added styling
