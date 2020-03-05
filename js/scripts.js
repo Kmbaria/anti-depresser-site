083c902de5ac28106341e5acae4fe946ba4db8b0
Lenny Makori <makorilenny28@gmail.com>
2020-03-05 12:27:32 +0300

    solving merge conflicts

--- js/scripts.js
+++ js/scripts.js
@@ -1,15 +1,4 @@
 $(document).ready(function(){
-<<<<<<< HEAD
-  $("h2#hmajor").click(function(){
-    $("#ulmajor").toggle();
-    $("#hhmajor").toggle();
-  })
-  $("h2#hpersistent").click(function(){
-    $("#ulpersistent").toggle();
-    $("#hhpersistent").toggle();
-  })
-  $("h2#hmanic").click(function(){
-=======
   $("h3#hmajor").click(function(){
     $("#ulmajor").toggle();
     $("#hhmajor").toggle();
@@ -19,7 +8,6 @@ $(document).ready(function(){
     $("#hhpersistent").toggle();
   })
   $("h3#hmanic").click(function(){
->>>>>>> 11db29b2e257e5a35577b943ec7bc9ab2529c2fc
     $("#hhmanic").toggle();
     $("#hhbipolar").toggle();
   })
@@ -43,11 +31,7 @@ $(document).ready(function(){
   $("#spsignsperinatal").click(function(){
     $("#ulsignsperinatal").toggle();
   })
-<<<<<<< HEAD
-  $("h2#hseasonal").click(function(){
-=======
   $("h3#hseasonal").click(function(){
->>>>>>> 11db29b2e257e5a35577b943ec7bc9ab2529c2fc
     $("#hhseasonal").toggle();
     $("#hhfall").toggle();
     $("#pfall").toggle();
@@ -61,19 +45,11 @@ $(document).ready(function(){
   $("#spspring").click(function(){
     $("#ulspring").toggle();
   })
-<<<<<<< HEAD
-  $("h2#hpremenstrual").click(function(){
-    $("#ulpremenstrual").toggle();
-    $("#hhpremenstrual").toggle();
-  })
-  $("h2#hsituational").click(function(){
-=======
   $("h3#hpremenstrual").click(function(){
     $("#ulpremenstrual").toggle();
     $("#hhpremenstrual").toggle();
   })
   $("h3#hsituational").click(function(){
->>>>>>> 11db29b2e257e5a35577b943ec7bc9ab2529c2fc
     $("#hheventsituational").toggle();
     $("#hhsituational").toggle();
   })
